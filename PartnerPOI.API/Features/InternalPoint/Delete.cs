446a56326b7844b9c5d126f8f02f6d8f0b59bfaa
AIT-thanhlnt <thanhlnt@aureole-it.vn>
2023-02-16 16:08:47 +0700

    update

---
+++ PartnerPOI.API/Features/InternalPoint/Delete.cs
@@ -0,0 +1,62 @@
+﻿using FastEndpoints;
+using PartnerPOI.API.Data;
+using PartnerPOI.API.DTOs;
+
+namespace PartnerPOI.API.Features.InternalPoint;
+
+public class Delete
+{
+    public class Request: BaseRequest
+    {
+        public string requestUserID { get; set; }
+        public string serviceIdentifiedByPartner { get; set; }
+        public string partnerPoinID { get; set; }
+        public string customerJourneyID { get; set; }
+    }
+    public class Response : BaseResponse { }
+    public class Validator: Validator<Request> { }
+    public class Endpoint: Endpoint<Request>
+    {
+        private readonly PartnerPOIContext _dbContext;
+        public Endpoint(PartnerPOIContext dbContext) => _dbContext = dbContext;
+        public override void Configure()
+        {
+            Post("/APFC8013/DeleteInternalPointSetting");
+            AllowAnonymous();
+            DontCatchExceptions();
+        }
+        public override async Task HandleAsync(Request req, CancellationToken ct)
+        {
+            var internalPoint = _dbContext.TbMInternalPointH.SingleOrDefault(
+                    x => x.serviceIdentifiedByPartner.Equals(req.serviceIdentifiedByPartner)
+                        && x.isDeleted == false);
+
+            if(internalPoint == null)
+            {
+                var m = $"Internal point not found by given id = {req.partnerPoinID}";
+                await SendAsync(new Response
+                {
+                    StatusCode = "000",
+                    Message = m
+                }, cancellation: ct);
+                return;
+            }
+
+            internalPoint.isDeleted = true;
+            internalPoint.updatedDate= DateTime.UtcNow;
+            internalPoint.updatedBy = req.UserID;
+
+            _dbContext.TbMInternalPointH.Update(internalPoint);
+            await _dbContext.SaveChangesAsync(ct);
+
+            var response = new Response
+            {
+                StatusCode = "000",
+                Message = "Successfully",
+            };
+
+            await SendAsync(response, cancellation: ct);
+
+        }
+    }
+}
---
+++ PartnerPOI.API/Features/InternalPoint/Update.cs
@@ -0,0 +1,69 @@
+﻿using FastEndpoints;
+using PartnerPOI.API.Data;
+using PartnerPOI.API.DTOs;
+
+namespace PartnerPOI.API.Features.InternalPoint;
+
+public class Update
+{
+    public class Event
+    {
+        public int unitPerPoint { get; set; }
+        public string expireDatePattern { get; set; } = null!;
+        public int multiplier { get; set; }
+        public string effectiveFrom { get; set; } = null!;
+        public string effectiveTo { get; set; } = null!;
+    }
+    public class Request: BaseRequest
+    {
+        public string requestUserID { get; set; }
+        public string serviceIdentifiedByPartner { get; set; }
+        public string internalPointID { get; set; }
+        public string customerLevel { get; set; }
+        public Event[] eventList { get; set; }
+
+    }
+    public class Response: BaseResponse { }
+    public class Validator: Validator<Request> { }
+    public class Endpoint: Endpoint<Request>
+    {
+        private readonly PartnerPOIContext _dbContext;
+        public Endpoint(PartnerPOIContext dbContext) => _dbContext = dbContext;
+        public override void Configure()
+        {
+            Put("/APFC8012/EditInternalPointSetting");
+            AllowAnonymous();
+            DontCatchExceptions();
+        }
+        public override async Task HandleAsync(Request req, CancellationToken ct)
+        {
+            var internalPoint = _dbContext.TbMInternalPointH.SingleOrDefault(
+                x => x.serviceIdentifiedByPartner.Equals(req.serviceIdentifiedByPartner)
+                && x.internalPointID.Equals(req.internalPointID)
+                && x.customerLevel.Equals(req.customerLevel)
+             );
+            if(internalPoint == null)
+            {
+                var m = $"Internal Point not founded by given id = {req.internalPointID}";
+                await SendAsync(new Response
+                {
+                    StatusCode = "000",
+                    Message = m
+                }, cancellation: ct);
+                return;
+            }
+
+            internalPoint.updatedDate = DateTime.UtcNow;
+            internalPoint.updatedBy = req.UserID;
+
+            _dbContext.TbMInternalPointH.Update(internalPoint);
+            await _dbContext.SaveChangesAsync(ct);
+            var response = new Response
+            {
+                StatusCode = "000",
+                Message = "Successfully"
+            };
+            await SendAsync(response, cancellation: ct);
+        }
+    }
+}
